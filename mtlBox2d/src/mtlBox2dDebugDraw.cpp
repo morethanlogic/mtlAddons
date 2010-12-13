74065b552aab6b972edffd50da7c078b675d0e3b
Elie Zananiri <ez@silentlycrashing.net>
2010-12-12 22:56:28 -0500

    Updated debug draw to work on iPhone.

--- mtlBox2d/src/mtlBox2dDebugDraw.cpp
+++ mtlBox2d/src/mtlBox2dDebugDraw.cpp
@@ -21,113 +21,102 @@ mtlBox2dDebugDraw::mtlBox2dDebugDraw() {
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawPolygon(const b2Vec2 *vertices, int32 vertexCount, const b2Color &color) {
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < vertexCount; i++) {
-        glVertex2f(M2PIX(vertices[i].x), M2PIX(vertices[i].y));
+    GLfloat	verts[vertexCount*2];
+    for (int32 i = 0; i < vertexCount; i++) {
+        verts[i*2+0] = M2PIX(vertices[i].x);
+		verts[i*2+1] = M2PIX(vertices[i].y);
     }
-    glEnd();
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+	
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawSolidPolygon(const b2Vec2 *vertices, int32 vertexCount, const b2Color &color) {
-    glColor4f(color.r, color.g, color.b, .5f);
-    glBegin(GL_TRIANGLE_FAN);
-    for (int i = 0; i < vertexCount; i++) {
-        glVertex2f(M2PIX(vertices[i].x), M2PIX(vertices[i].y));
+    GLfloat	verts[vertexCount*2];
+    for (int32 i = 0; i < vertexCount; i++) {
+        verts[i*2+0] = M2PIX(vertices[i].x);
+		verts[i*2+1] = M2PIX(vertices[i].y);
     }
-    glEnd();
+    glVertexPointer(2, GL_FLOAT, 0, verts);
     
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < vertexCount; ++i) {
-        glVertex2f(M2PIX(vertices[i].x), M2PIX(vertices[i].y));
-    }
-    glEnd();
+    glColor4f(color.r, color.g, color.b, .5f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, vertexCount);
+    
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawCircle(const b2Vec2 &center, float32 radius, const b2Color &color) {
     float ang = 0.f;
-    
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < kCircleSegments; i++) {
+    GLfloat	verts[kCircleSegments*2];
+    for (int32 i = 0; i < kCircleSegments; i++) {
         b2Vec2 v = M2PIX(center) + M2PIX(radius) * b2Vec2(cosf(ang), sinf(ang));
-        glVertex2f(v.x, v.y);
+        verts[i*2+0] = v.x;
+		verts[i*2+1] = v.y;
         ang += kCircleSlice;
     }
-    glEnd();
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+	
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, kCircleSegments);
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawSolidCircle(const b2Vec2 &center, float32 radius, const b2Vec2 &axis, const b2Color &color) {
     float ang = 0.f;
-    
-    glColor4f(color.r, color.g, color.b, .5f);
-//    ofCircle(M2PIX(center.x), M2PIX(center.y), M2PIX(radius));
-    glBegin(GL_TRIANGLE_FAN);
+    GLfloat	verts[kCircleSegments*2];
     for (int32 i = 0; i < kCircleSegments; i++) {
         b2Vec2 v = M2PIX(center) + M2PIX(radius) * b2Vec2(cosf(ang), sinf(ang));
-        glVertex2f(v.x, v.y);
+        verts[i*2+0] = v.x;
+		verts[i*2+1] = v.y;
         ang += kCircleSlice;
     }
-    glEnd();
-    
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINE_LOOP);
-    for (int i = 0; i < kCircleSegments; i++) {
-        b2Vec2 v = M2PIX(center) + M2PIX(radius) * b2Vec2(cosf(ang), sinf(ang));
-        glVertex2f(v.x, v.y);
-        ang += kCircleSlice;
-    }
-    glEnd();
+    glVertexPointer(2, GL_FLOAT, 0, verts);
 
+    glColor4f(color.r, color.g, color.b, .5f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, kCircleSegments);
+    
+    glColor4f(color.r, color.g, color.b, 1.f);
+	glDrawArrays(GL_LINE_LOOP, 0, kCircleSegments);
+	
 	// draw the axis line
 	DrawSegment(center, center + radius*axis, color);
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawSegment(const b2Vec2 &p1, const b2Vec2 &p2, const b2Color &color) {
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINES);
-    {
-        glVertex2f(M2PIX(p1.x), M2PIX(p1.y));
-        glVertex2f(M2PIX(p2.x), M2PIX(p2.y));
-    }
-    glEnd();
+    GLfloat	verts[] = { M2PIX(p1.x), M2PIX(p1.y), 
+                        M2PIX(p2.x), M2PIX(p2.y) };
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+	
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_LINES, 0, 2);
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawTransform(const b2Transform &xf) {
-    b2Vec2 p1 = M2PIX(xf.position);
+    b2Vec2 p1 = xf.position;
     b2Vec2 p2;
     
-    glBegin(GL_LINES);
-    {
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(p1.x, p1.y);
-        p2 = p1 + M2PIX(kAxisScale * xf.R.col1);
-        glVertex2f(p2.x, p2.y);
-        
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(p1.x, p1.y);
-        p2 = p1 + M2PIX(kAxisScale * xf.R.col2);
-        glVertex2f(p2.x, p2.y);
-    }
-    glEnd();
+    p2 = p1 + kAxisScale * xf.R.col1;
+	DrawSegment(p1, p2, b2Color(1, 0, 0));
+	
+	p2 = p1 + kAxisScale * xf.R.col2;
+	DrawSegment(p1, p2, b2Color(0, 1, 0));
 }
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawPoint(const b2Vec2 &p, float32 size, const b2Color &color) {
-    glColor3f(color.r, color.g, color.b);
+    GLfloat	verts[] = { M2PIX(p.x), M2PIX(p.y) };
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+    
     glPointSize(size);
-    glBegin(GL_POINTS);
-    {
-        glVertex2f(M2PIX(p.x), M2PIX(p.y));
-    }
-    glEnd();
-    glPointSize(1.0f);
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_POINTS, 0, 1);
+    glPointSize(1.f);
 }
 
 //--------------------------------------------------------------
@@ -165,15 +154,14 @@ void mtlBox2dDebugDraw::DrawString(int x, int y, const char *string, ...) {
 
 //--------------------------------------------------------------
 void mtlBox2dDebugDraw::DrawAABB(b2AABB *aabb, const b2Color &color) {
-    glColor3f(color.r, color.g, color.b);
-    glBegin(GL_LINE_LOOP);
-    {
-        glVertex2f(M2PIX(aabb->lowerBound.x), M2PIX(aabb->lowerBound.y));
-        glVertex2f(M2PIX(aabb->upperBound.x), M2PIX(aabb->lowerBound.y));
-        glVertex2f(M2PIX(aabb->upperBound.x), M2PIX(aabb->upperBound.y));
-        glVertex2f(M2PIX(aabb->lowerBound.x), M2PIX(aabb->upperBound.y));
-    }
-    glEnd();
+    GLfloat	verts[] = { M2PIX(aabb->lowerBound.x), M2PIX(aabb->lowerBound.y),
+                        M2PIX(aabb->upperBound.x), M2PIX(aabb->lowerBound.y),
+                        M2PIX(aabb->upperBound.x), M2PIX(aabb->upperBound.y),
+                        M2PIX(aabb->lowerBound.x), M2PIX(aabb->upperBound.y) };
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+	
+    glColor4f(color.r, color.g, color.b, 1.f);
+    glDrawArrays(GL_LINE_LOOP, 0, 8);
 }
 
 
